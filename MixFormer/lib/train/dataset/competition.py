f01b22e34e3794ce2ceb3d26778e750f95d6c123
memoryunreal <814514103@qq.com>
2022-08-09 19:21:16 +0800

    training_v1

--- MixFormer/lib/train/dataset/competition.py
+++ MixFormer/lib/train/dataset/competition.py
@@ -64,27 +64,12 @@ class competition(BaseVideoDataset):
 
             self.seq_per_class = self._build_class_list()
 
-    # def _build_sequence_list(self, vid_ids=None, split=None):
-    #     if split is not None:
-    #         if vid_ids is not None:
-    #             raise ValueError('Cannot set both split_name and vid_ids.')
-    #         ltr_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
-    #         if split == 'train':
-    #             file_path = os.path.join(ltr_path, 'data_specs', 'lasot_train_split.txt')
-    #         else:
-    #             raise ValueError('Unknown split name.')
-    #         sequence_list = pandas.read_csv(file_path, header=None, squeeze=True).values.tolist()
-    #     elif vid_ids is not None:
-    #         sequence_list = [c+'-'+str(v) for c in self.class_list for v in vid_ids]
-    #     else:
-    #         raise ValueError('Set either split_name or vid_ids.')
-
-    #     return sequence_list
 
     def _build_class_list(self):
         seq_per_class = {}
         for seq_id, seq_name in enumerate(self.sequence_list):
-            class_name = seq_name.split('-')[0]
+            # class_name = seq_name.split('-')[0]
+            class_name = seq_name
             if class_name in seq_per_class:
                 seq_per_class[class_name].append(seq_id)
             else:
@@ -113,14 +98,17 @@ class competition(BaseVideoDataset):
     # competition groundtruth
     def _read_bb_anno(self, seq_path):
         
-        bb_anno_file = os.path.join(seq_path, "groundtruth.txt")
+        # bb_anno_file = os.path.join(seq_path, "groundtruth.txt")
+        bb_anno_file = os.path.join(seq_path, "..", "..", "..", "attribute","groundtruth", "groundtruth_{}.txt".format(os.path.basename(seq_path).split("_")[1]))
         gt = pandas.read_csv(bb_anno_file, delimiter=',', header=None, dtype=np.float32, na_filter=False, low_memory=False).values
         return torch.tensor(gt)
 
     def _read_target_visible(self, seq_path):
         # Read full occlusion and out_of_view
-        occlusion_file = os.path.join(seq_path, "full_occlusion.txt")
-        out_of_view_file = os.path.join(seq_path, "out_of_view.txt")
+        out_of_view_file = os.path.join(seq_path, "..", "..", "..", "attribute","absent", "absent_{}.txt".format(os.path.basename(seq_path).split("_")[1]))
+        occlusion_file = os.path.join(seq_path, "..", "..", "..", "attribute","occlusion", "occlusion_{}.txt".format(os.path.basename(seq_path).split("_")[1]))
+        # occlusion_file = os.path.join(seq_path, "full_occlusion.txt")
+        # out_of_view_file = os.path.join(seq_path, "out_of_view.txt")
 
         with open(occlusion_file, 'r', newline='') as f:
             occlusion = torch.ByteTensor([int(v) for v in list(csv.reader(f))[0]])
@@ -133,10 +121,11 @@ class competition(BaseVideoDataset):
 
     def _get_sequence_path(self, seq_id):
         seq_name = self.sequence_list[seq_id]
-        class_name = seq_name.split('-')[0]
-        vid_id = seq_name.split('-')[1]
+        class_name = seq_name
+        vid_id = seq_name.split('_')[1]
 
-        return os.path.join(self.root, class_name, class_name + '-' + vid_id)
+        # return os.path.join(self.root, class_name, class_name + '-' + vid_id)
+        return os.path.join(self.root, class_name)
 
     def get_sequence_info(self, seq_id):
         seq_path = self._get_sequence_path(seq_id)
@@ -148,13 +137,13 @@ class competition(BaseVideoDataset):
         return {'bbox': bbox, 'valid': valid, 'visible': visible}
 
     def _get_frame_path(self, seq_path, frame_id):
-        return os.path.join(seq_path, 'img', '{:08}.jpg'.format(frame_id+1))    # frames start from 1
+        return os.path.join(seq_path, '{:08}.jpg'.format(frame_id+1))    # frames start from 1
 
     def _get_frame(self, seq_path, frame_id):
         return self.image_loader(self._get_frame_path(seq_path, frame_id))
 
     def _get_class(self, seq_path):
-        raw_class = seq_path.split('/')[-2]
+        raw_class = seq_path.split('/')[-1]
         return raw_class
 
     def get_class_name(self, seq_id):
